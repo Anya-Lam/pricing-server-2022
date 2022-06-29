f772122ea999cc419621328978e4df00ff7ae930
Kelvin Macartney <kmacartney@expediagroup.com>
2022-06-29 16:59:20 +0100

    Add hosting instructions

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
@@ -6,25 +5,23 @@ app = Flask(__name__)
 CORS(app)
 
 pricing_database = {
-    'ritz': '560'
+    'theritz': '560'
 }
 
 reviews_database = {
     'ritz': [2, 4, 6, 7, 6, 5, 7]
 }
 
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello POO!'
+    return 'Hello World!'
+
 
 @app.route('/hotel/price/<hotel_id>')
 @cross_origin(origin='*')
 def get_hotel_price(hotel_id):
-    price = pricing_database.get('theritz')
-    if price != None:
-        return price
-    else:
-        return 'No price found :('
+    return 'No price found :('
 
 
 @app.route('/hotel/reviews/<hotel_id>')
